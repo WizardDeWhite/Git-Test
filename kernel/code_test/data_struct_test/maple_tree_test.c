5387ca0b23313a144130cab41f10b195f05d0567
Wei Yang <richard.weiyang@gmail.com>
2024-07-16 09:41:01 +0800

    kernel/data_struct: maple tree test skeleton

--- kernel/code_test/data_struct_test/maple_tree_test.c
+++ kernel/code_test/data_struct_test/maple_tree_test.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include "test_common.h"
 #include "maple_tree.h"
 
-int main()
+void maple_tree_test()
 {
+	int i;
+
+	prefix_reset();
+	prefix_push("maple_tree_test");
+	test_print("Running %s tests...\n", __func__);
+
+	PREFIX_PUSH();
+
+	test_pass_pop();
+
+	prefix_pop();
+}
+
+int main(int argc, char *argv[])
+{
+	parse_args(argc, argv);
+
+	maple_tree_test();
+
 	return 0;
 }
